8504f29cb0f0c206d03641c5ea3d49c02fa73780
J3rmed <jeffersonlizarazo0258@gmail.com>
2024-10-16 16:03:59 -0500

    Adding video-views structures

---
+++ src/main/java/culturemedia/exception/CulturemediaException.java
@@ -0,0 +1,7 @@
+package culturemedia.exception;
+
+public class CulturemediaException extends RuntimeException {
+    public CulturemediaException(String message) {
+        super(message);
+    }
+}
